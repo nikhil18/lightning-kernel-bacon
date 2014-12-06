a1abb8cc25be2b955c3e2935d16facba242d6cc7
Olav Haugan <ohaugan@codeaurora.org>
2014-12-06 10:09:43 -0800

    sched: Avoid frequent task migration due to EA in lb
    A new tunable exists that allow task migration to be throttled when the
    scheduler tries to do task migrations due to Energy Awareness (EA). This
    tunable is only taken into account when migrations occur in the tick
    path. Extend the usage of the tunable to take into account the load
    balancer (lb) path also.
    
    In addition ensure that the start of task execution on a CPU is updated
    correctly. If a task is preempted but still runnable on the same CPU the
    start of execution should not be updated. Only update the start of
    execution when a task wakes up after sleep or moves to a new CPU.
    
    Signed-off-by: Olav Haugan <ohaugan@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2677,7 +2677,7 @@ void set_task_cpu(struct task_struct *p, unsigned int new_cpu)
 
 	trace_sched_migrate_task(p, new_cpu, pct_task_load(p));
 
-	note_run_start(p, -1);
+	note_run_start(p, sched_clock());
 
 	if (task_cpu(p) != new_cpu) {
 		struct task_migration_notifier tmn;
@@ -3232,6 +3232,8 @@ try_to_wake_up(struct task_struct *p, unsigned int state, int wake_flags)
 	if (src_cpu != cpu) {
 		wake_flags |= WF_MIGRATED;
 		set_task_cpu(p, cpu);
+	} else {
+		note_run_start(p, wallclock);
 	}
 #endif /* CONFIG_SMP */
 
@@ -4767,7 +4769,6 @@ need_resched:
 			prev->state = TASK_RUNNING;
 		} else {
 			deactivate_task(rq, prev, DEQUEUE_SLEEP);
-			note_run_start(prev, -1);
 			prev->on_rq = 0;
 
 			/*
@@ -4798,7 +4799,6 @@ need_resched:
 	update_task_ravg(next, rq, PICK_NEXT_TASK, wallclock, 0);
 	clear_tsk_need_resched(prev);
 	rq->skip_clock_update = 0;
-	note_run_start(next, wallclock);
 
 	BUG_ON(task_cpu(next) != cpu_of(rq));
 
