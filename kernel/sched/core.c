cf0663222e46f1494c82c9613d760bf4d80dfacf
Steve Muckle <smuckle@codeaurora.org>
2015-04-24 17:03:31 -0700

    sched: disable IRQs in update_min_max_capacity
    IRQs must be disabled while locking runqueues since an
    interrupt may cause a runqueue lock to be acquired.
    
    Signed-off-by: Steve Muckle <smuckle@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2360,8 +2360,10 @@ static void __update_min_max_capacity(void)
 
 static void update_min_max_capacity(void)
 {
+	unsigned long flags;
 	int i;
 
+	local_irq_save(flags);
 	for_each_possible_cpu(i)
 		raw_spin_lock(&cpu_rq(i)->lock);
 
@@ -2369,6 +2371,7 @@ static void update_min_max_capacity(void)
 
 	for_each_possible_cpu(i)
 		raw_spin_unlock(&cpu_rq(i)->lock);
+	local_irq_restore(flags);
 }
 
 /*
