4f174cfcd571e1be3c13fc31faaeb47763ba7b78
Pranav Vashi <neobuddy89@gmail.com>
2016-10-25 00:31:37 +0530

    msm: Use 64 bit type for sclk
    'us' can overflow and can potentially cause unexpected wakeup.
    
    Change it to uint64_t.
    
    Change-Id: I943cbc9d62268ca073e388c287e3b180c0eaa8e3
    Signed-off-by: Girish S Ghongdemath <girishsg@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- arch/arm/mach-msm/lpm_levels.c
+++ arch/arm/mach-msm/lpm_levels.c
@@ -362,7 +362,7 @@ static void lpm_system_prepare(struct lpm_system_state *system_state,
 	struct lpm_system_level *lvl;
 	uint32_t sclk;
 	int ret;
-	int64_t us = (~0ULL);
+	uint64_t us = (~0ULL);
 	int dbg_mask;
 	struct cpumask nextcpu;
 
@@ -400,7 +400,7 @@ static void lpm_system_prepare(struct lpm_system_state *system_state,
 	}
 
 	do_div(us, USEC_PER_SEC/SCLK_HZ);
-	sclk = (uint32_t)us;
+	sclk = us;
 	msm_mpm_enter_sleep(sclk, from_idle, &nextcpu);
 	if (from_idle)
 		time = sched_clock();
